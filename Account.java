a315cb3e6867e46cad030b59ba22b3bfef343757
Yaswanthamasa <70936970+Yaswanthamasa@users.noreply.github.com>
2020-11-11 23:22:51 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -3,7 +3,6 @@ package Atm;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Scanner;
 class Account {
 	 private int AccountNumber;
 	 private String name;
